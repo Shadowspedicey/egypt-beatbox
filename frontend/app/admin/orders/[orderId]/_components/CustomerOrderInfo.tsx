74edbb3bf6421c99f976fe934fe23a523c22ca5e
Ahmed Khaled <shadowspedicey@gmail.com>
2026-01-07 13:58:43 +0200

    frontend - Remove edit button from customer and order info box

--- frontend/app/admin/orders/[orderId]/_components/CustomerOrderInfo.tsx
+++ frontend/app/admin/orders/[orderId]/_components/CustomerOrderInfo.tsx
@@ -4,14 +4,11 @@ import { format } from "date-fns";
 export default function CustomerOrderInfo({order}: {order: IOrder}) {
 	return (
 		<div className="bg-surface-dark rounded-lg border border-white/5 overflow-hidden">
-			<div className="p-5 border-b border-white/5 flex justify-between items-center bg-white/[0.02]">
+			<div className="p-5 border-b border-white/5 flex justify-between items-center bg-white/2">
 				<h3 className="text-lg font-bold text-white flex items-center gap-2">
 					<span className="material-symbols-outlined text-primary">person</span>
 					Customer &amp; Order Info
 				</h3>
-				<button className="text-gray-400 hover:text-white text-sm flex items-center gap-1">
-					<span className="material-symbols-outlined text-[16px]">edit</span> Edit
-				</button>
 			</div>
 			<div className="p-6 grid grid-cols-1 md:grid-cols-2 gap-y-8 gap-x-6">
 				<div className="flex flex-col gap-1.5">
