94f1146bc6069d568e9b8c1e0ccba3a0f6e7d429
Ahmed Khaled <shadowspedicey@gmail.com>
2026-01-08 22:48:12 +0200

    backend - Load users' emails in recent orders of dashboard

--- backend/EgyptBeatbox.Application/Admins/AdminService.cs
+++ backend/EgyptBeatbox.Application/Admins/AdminService.cs
@@ -1,13 +1,15 @@
 ﻿using EgyptBeatbox.Application.Mappings;
 using EgyptBeatbox.Application.Repositories;
+using EgyptBeatbox.Application.Users;
 using EgyptBeatbox.Domain.Entities.Orders;
 using FluentResults;
 
 namespace EgyptBeatbox.Application.Admins
 {
-	public class AdminService(IUnitOfWork unitOfWork) : IAdminService
+	public class AdminService(IUnitOfWork unitOfWork, IUserManager userManager) : IAdminService
 	{
 		private readonly IUnitOfWork _unitOfWork = unitOfWork;
+		private readonly IUserManager _userManager = userManager;
 
 		public async Task<Result<ViewDashboardDTO>> ViewDashboardDTO()
 		{
@@ -16,6 +18,8 @@ namespace EgyptBeatbox.Application.Admins
 			var totalRevenue = orderItems.Sum(oi => oi.TotalPrice.Amount);
 			int pendingApprovals = await _unitOfWork.Orders.Count(o => o.Status == OrderStatus.Pending);
 			var recentOrders = await _unitOfWork.Orders.GetLastN(5, orderingClause: o => o.CreatedAt);
+			foreach (var user in recentOrders.Select(o => o.Customer).DistinctBy(c => c.Id))
+				await _userManager.LoadEmail(user);
 			return new ViewDashboardDTO(totalTicketsSold, totalRevenue, pendingApprovals, recentOrders.Select(o => o.ToViewOrderDTO()));
 		}
 	}
