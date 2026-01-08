1c6140cb52f0509817feb18998b25e8520b8e944
Ahmed Khaled <shadowspedicey@gmail.com>
2026-01-08 23:12:01 +0200

    backend - Remove extra FK from AspUserRoles table (Migration)

---
+++ backend/EgyptBeatbox.Infrastructure/Migrations/20260108210631_RemoveExtraFKUserRoles.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EgyptBeatbox.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveExtraFKUserRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetRoles_AspNetUsers_AppUserId",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetRoles_AppUserId",
+                table: "AspNetRoles");
+
+            migrationBuilder.DropColumn(
+                name: "AppUserId",
+                table: "AspNetRoles");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "AppUserId",
+                table: "AspNetRoles",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"),
+                column: "AppUserId",
+                value: null);
+
+            migrationBuilder.UpdateData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: new Guid("22222222-2222-2222-2222-222222222222"),
+                column: "AppUserId",
+                value: null);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetRoles_AppUserId",
+                table: "AspNetRoles",
+                column: "AppUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetRoles_AspNetUsers_AppUserId",
+                table: "AspNetRoles",
+                column: "AppUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
